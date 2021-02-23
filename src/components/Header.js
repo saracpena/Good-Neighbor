014fda47690170eafb7a2e92553c04f49bcb27e3
Sara Peña <s.cristal.p@gmail.com>
2021-02-23 00:12:08 -0500

    Header-Left

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,42 @@
 import React from 'react'
+import styled from "styled-components"
+import { Avatar } from '@material-ui/core'
+import AccessTimeIcon from '@material-ui/icons/AccessTime';
 
 function Header() {
     return (
-        <div>
-            
-        </div>
+        <HeaderContainer>
+            <HeaderLeft>
+              <HeaderAvatar
+              //TODO: Add onClick   
+              />
+              <AccessTimeIcon />
+            </HeaderLeft>
+          
+           {/* Header Search */}
+
+           {/* Header Right */}
+        </HeaderContainer>
     )
 }
 
 export default Header
+
+// Using Styled Components
+const HeaderContainer = styled.div`
+    display: flex;
+
+`;
+const HeaderLeft = styled.div` //h2
+    flex: 0.3;
+    display: flex;
+    align-items: center;
+    margin-left: 20px;
+
+    >   .MuiSvgIcon-root {
+      margin-left: auto;
+      margin-right: 30px;  
+    }
+`;
+const HeaderAvatar = styled(Avatar)`
+`

16254c171a29fd64eb4159a06e26da196619f606
Sara Peña <s.cristal.p@gmail.com>
2021-02-28 21:55:03 -0500

    update folder setup

---
+++ Back End/src/test/java/com/goodneighbor/GoodNeighborTests.java
@@ -0,0 +1,13 @@
+package com.goodneighbor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodNeighborTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

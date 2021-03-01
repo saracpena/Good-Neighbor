dc8f089f580ce48dc10a5636517a388e6058ef79
Sara Peña <s.cristal.p@gmail.com>
2021-02-28 21:52:05 -0500

    Update pom.xml

---
+++ Front End/src/components/User.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import UserData from './UserData'
+
+function User() {
+    
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default User

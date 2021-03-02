9ac12b32a317a720ee45744cf037998a4b0ad608
Sara Peña <s.cristal.p@gmail.com>
2021-03-01 20:04:45 -0500

    BE update

--- Back End/target/classes/data-postgres.sql
+++ Back End/target/classes/data-postgres.sql
@@ -2,10 +2,10 @@ INSERT INTO cities(name) VALUES('Boston');
 INSERT INTO cities(name) VALUES('Los Angeles');
 INSERT INTO cities(name) VALUES('New York');
 
-INSERT INTO neighbors(name, address1, address2, city, state, zip, phone) VALUES('John McCarthy', '234 Border St', 'Apt. 209', 'East Boston', 'Massachusetts(MA)', '02128', '617-205-1343');
-INSERT INTO neighbors(name, address1, address2, city, state, zip, phone) VALUES('Kate Hopkins', '95 Jeffries Street', '', 'East Boston', 'Massachusetts(MA)', '02128', '617-217-4113');
-INSERT INTO neighbors(name, address1, address2, city, state, zip, phone) VALUES('Pal Ryall', '428 Webster St', 'Apt. 3', 'East Boston', 'Massachusetts(MA)', '02128', '617-236-6272');
-INSERT INTO neighbors(name, address1, address2, city, state, zip, phone) VALUES('Buddy Shanahan', '32 Marginal Street', '', 'East Boston', 'Massachusetts(MA)', '02128', '617-261-7284');
-INSERT INTO neighbors(name, address1, address2, city, state, zip, phone) VALUES('Compadre Cummins', '409 Maverick St', 'Apt. 5', 'East Boston', 'Massachusetts(MA)', '02128', '617-623-4860');
+INSERT INTO neighbors(name, address1, address2, city, state, zip, phone) VALUES('John McCarthy', '234 Border St', 'Apt. 209', 'East Boston', 'Massachusetts', '02128', '617-205-1343');
+INSERT INTO neighbors(name, address1, address2, city, state, zip, phone) VALUES('Kate Hopkins', '95 Jeffries Street', '', 'East Boston', 'Massachusetts', '02128', '617-217-4113');
+INSERT INTO neighbors(name, address1, address2, city, state, zip, phone) VALUES('Pal Ryall', '428 Webster St', 'Apt. 3', 'East Boston', 'Massachusetts', '02128', '617-236-6272');
+INSERT INTO neighbors(name, address1, address2, city, state, zip, phone) VALUES('Buddy Shanahan', '32 Marginal Street', '', 'East Boston', 'Massachusetts', '02128', '617-261-7284');
+INSERT INTO neighbors(name, address1, address2, city, state, zip, phone) VALUES('Compadre Cummins', '409 Maverick St', 'Apt. 5', 'East Boston', 'Massachusetts', '02128', '617-623-4860');
 
 INSERT INTO disclaimer(header, body) VALUES('Disclaimer', 'The words of which the initial letter is capitalized have meanings defined under the following conditions. The following definitions shall have the same meaning regardless of whether they appear in singular or in plural.');
